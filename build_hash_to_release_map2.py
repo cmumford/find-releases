1af11c8108d5bb92b60cd563a98790bee3ed0dee
Scott Graham <scottmg@chromium.org>
2016-10-22 09:34:59 -0700

    attempt all history via ulimit

--- build_hash_to_release_map2.py
+++ build_hash_to_release_map2.py
@@ -34,7 +34,8 @@ def main(args):
 
   sha1_to_release = cache['sha1_to_release']
   print 'retrieving list of commits...'
-  revs = set(Git('rev-list', '--all', '--after=\'20 months ago\'').splitlines())
+  # If this crashes, `ulimit -s unlimited`.
+  revs = set(Git('rev-list', '--all').splitlines())
   have_rev_data = set(sha1_to_release.keys())
   revs_to_get = revs - have_rev_data
   print 'getting revs for %d commits...' % len(revs_to_get)
