84b2a3da822130b66362c82f6547e2d6aa906c5b
Scott Graham <scottmg@chromium.org>
2016-10-24 14:22:50 -0700

    formatting

--- handler.js
+++ handler.js
@@ -13,14 +13,14 @@ function go() {
     var commit = window.location.hash.substring(1);
     var commit_data = null;
     if (!data.hasOwnProperty(commit)) {
-      var html_data = '<p>commit ' + wrap_commit(commit) + ' data not found</p>';
-      html_data += '<p>looking through prefix short commits...</p>';
+      var html_data = '<p>Commit "' + commit + '" data not found.</p>';
+      html_data += '<p>Looking at prefix short commits...</p>';
       // See if the given commit was just the beginning of a full SHA1.
       for (var candidate in data) {
         if (data.hasOwnProperty(candidate)) {
           if (candidate.startsWith(commit)) {
             if (commit_data) {
-              html_data += '<p>' + commit + ' is ambiguous.</p>';
+              html_data += '<p>"' + commit + '" is ambiguous.</p>';
               commit_data = null;
               break;
             }
@@ -33,7 +33,7 @@ function go() {
     }
 
     if (commit_data) {
-      html_data += '<p>commit ' + wrap_commit(commit);
+      html_data += '<p>Commit ' + wrap_commit(commit);
       html_data += ' initially landed in <code>' + commit_data[0] + '</code></p>';
       var num_merges = commit_data[1].length;
       if (num_merges == 0) {
@@ -43,13 +43,13 @@ function go() {
         for (var i = 0; i < num_merges; ++i) {
           var merge = commit_data[1][i];
           html_data += '<li>Merged to <code>' + merge[0] + '</code>';
-          html_data += ' (as ' + wrap_commit(merge[1]) + ')</li>';
+          html_data += ' (as ' + wrap_commit(merge[1]) + ').</li>';
         }
         html_data += '</ul>';
       }
     }
   } else {
-    html_data = 'no commit specified';
+    html_data = 'No commit specified.';
   }
   p.innerHTML = html_data;
   content.appendChild(p);
